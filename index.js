f86cc68a6b004b95af5e728ad912e95d986cec94
wesleyoliveira01 <wesleyoliveira.dev@outlook.com>
2023-06-23 16:09:39 -0300

    init

---
+++ index.js
@@ -0,0 +1,8 @@
+import Admin from "./Admin.js";
+import Docente from "./Docente.js";
+import User from "./User.js";
+
+const NovoUsuario = new User("Wesley","teste@teste","27-12-2001")
+
+console.log(NovoUsuario.exibirInfos());
+
